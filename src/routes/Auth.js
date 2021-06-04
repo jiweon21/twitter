66507883d868413a722868118209a13cc3f3b49a
jiweon21 <jna215843@gmail.com>
2021-06-04 13:37:16 +0900

    [Add]Log Out

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,8 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [err, setError] = useState("");
+
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if(name === "email") {
@@ -23,9 +25,8 @@ const Auth = () => {
                 const data =await authService.signInWithEmailAndPassword(email, password);
                 console.log(data);
             }
-
         } catch(err) {
-            console.log(err);
+            setError(err.message);
         }
     }
     const onSocialClick = async (event) => {
@@ -36,8 +37,7 @@ const Auth = () => {
         } else if(name === "github") {
             provider = new firebaseInstance.auth.GithubAuthProvider();
         }
-        const data = await authService.signInWithPopup(provider);
-        console.log(data)
+        await authService.signInWithPopup(provider);
     }
     const toggleAccount = () => {
         setNewAccount((prev) => !prev);
@@ -48,6 +48,7 @@ const Auth = () => {
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
                 <input type="submit" value={newAccount ?  "Create Account" : "Sign In"} />
+                {err}
             </form>
             <span onClick={toggleAccount}>
                 {newAccount ? "Sign In" : "Create Account"}
